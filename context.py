3a8a3f159aefe8c7605548e11de1edfaa28ee646
MMN <michael.naunton@gmail.com>
2017-03-10 05:15:20 -0500

    fix context badness

--- context.py
+++ context.py
@@ -2,17 +2,29 @@
 from noval import _noVal
 from monitor import Monitor
 
+class RootContext(object):
+    def __init__(self):
+        self.parent = None
+        self.tweaks = {}
+        self.cache = {}
+        self.name = 'Root'
+    def get(self, cmb):
+        return _noVal
+    def set(self, cmb, v):
+        self.cache[cmb] = v
+
 class Context(object):
-    _contexts = []
+    _contexts = [RootContext()]
     
     def __init__(self, tweaks, name=None):
-        p = self.parent()
-        self.tweaks = p.tweaks.copy() if p else {}
-        self.tweaks.update(tweaks)
-        self.cache = self.tweaks.copy()
+        p = self.current()
         self.name = name if name else '%s' % id(name)
         if p:
             self.name = '%s:%s' % (p.name, self.name)
+        Monitor.msg('Context', 0, 'create', ctx=self)
+        self.tweaks = p.tweaks.copy() if p else {}
+        self.tweaks.update(tweaks)
+        self.cache = self.tweaks.copy()
         
     def __enter__(self, *a):
         Monitor.msg('Context', 1, 'enter', ctx=self)
@@ -28,14 +40,7 @@ class Context(object):
   
     @classmethod
     def current(cls):
-        if not cls._contexts:
-            cls._contexts.append(Context({}))
         return cls._contexts[-1]
-    
-    @classmethod
-    def parent(cls):
-        if len(cls._contexts) > 1:
-            return cls._contexts[-2]
     
     @classmethod
     def inRootContext(cls):
