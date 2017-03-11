0ccc18da17aa035af88c07726dc88e014edc0116
MMN <michael.naunton@gmail.com>
2017-03-11 08:09:54 -0500

    checkpoint

--- node.py
+++ node.py
@@ -1,4 +1,6 @@
 
+from noval import _noVal
+
 class Node(object):
     def __init__(self, ctx, key, value):
         self.ctx = ctx
@@ -11,3 +13,13 @@ class Node(object):
         key = self.key
         ctx = self.ctx
         return '<%s@%x/%s in %s>' % (key[0].__class__.__name__, id(key[0]), key[1], ctx.name)
+
+    def printInputGraph(self, depth=0):
+        print '  '*depth, self
+        for i in self.inputs:
+            i.printInputGraph(depth+1)
+
+    def _invalidate(self):
+        self.value = _noVal
+        self.inputs = set()
+        self.outputs = set()
