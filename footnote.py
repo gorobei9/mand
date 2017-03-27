e80843a687619482693f1c407a22c11a7fe305f9
MMN <michael.naunton@gmail.com>
2017-03-27 16:03:03 -0400

    init ver

---
+++ footnote.py
@@ -0,0 +1,13 @@
+
+from graph import _getCurrentNode
+from dictutils import merge
+
+def addFootnote(text=None,
+                info=None,
+                node=None):
+    key = text
+    if node is None:
+        node = _getCurrentNode()
+
+    s = set([info]) if info else set()
+    merge(node.footnotes, { key: s }, deleteZeros=False)
